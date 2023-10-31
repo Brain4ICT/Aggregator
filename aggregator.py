a13c8c14c2d614c7640a57e4d405b57a795a5678
Wael Ouarda <135534995+Brain4ICT@users.noreply.github.com>
2023-10-31 17:55:02 +0100

    Update aggregator.py

--- aggregator.py
+++ aggregator.py
@@ -1,25 +1,17 @@
 import streamlit as st
 import pandas as pd
-import os
 import matplotlib.pyplot as plt
 import numpy as np
+import os
+from statsmodels.tsa.arima_model import ARIMA
 
-st.title("Analyse des Courbes de Charge et Détection des Outliers")
-
-# Fonction pour lire les fichiers Excel au format .xlsx
-def read_excel_file(file_path):
-    if file_path.endswith(".xlsx"):
-        df = pd.read_excel(file_path, engine="openpyxl")
-    else:
-        df = pd.read_excel(file_path)
-    return df
+st.title("Analyse des Courbes de Charge - Aggregators")
 
 # Fonction pour tracer les courbes de charge à partir des fichiers Excel
-def plot_load_curves(file_paths):
+def plot_load_curves(data_frames):
     fig, ax = plt.subplots(figsize=(10, 6))
-    for file_path in file_paths:
-        df = read_excel_file(file_path)
-        ax.plot(df['Datetime'], df['Load'], label=os.path.basename(file_path))
+    for df in data_frames:
+        ax.plot(df['Datetime'].values, df['Load'].values, label=df['Name'].iloc[0])
     ax.set_xlabel('Datetime')
     ax.set_ylabel('Load')
     ax.legend()
@@ -34,37 +26,28 @@ def detect_outliers(series, threshold=2.0):
 # Uploader un dossier contenant des fichiers Excel
 uploaded_folder = st.file_uploader("Uploader un dossier contenant des fichiers Excel", type=["xlsx"], accept_multiple_files=True, key="upload_folder")
 
-if uploaded_folder:
+data_frames = []
 
-    # Sauvegarder les fichiers Excel dans le répertoire temporaire
-    file_paths = []
+if uploaded_folder:
     for uploaded_file in uploaded_folder:
-        file_path = os.path.join(uploaded_file.name)
-        with open(file_path, 'wb') as f:
-            f.write(uploaded_file.read())
-        file_paths.append(file_path)
+        data = pd.read_excel(uploaded_file, engine="openpyxl")
+        data['Name'] = os.path.basename(uploaded_file.name)
+        data_frames.append(data)
 
     # Afficher les courbes de charge à partir des fichiers Excel
-    plot_load_curves(file_paths)
-
-    # Nettoyer le répertoire temporaire après utilisation
-    for file_path in file_paths:
-        os.remove(file_path)
+    plot_load_curves(data_frames)
 
     # Détection des outliers pour la courbe globale
     st.header("Détection des Outliers pour la Courbe Globale")
-    global_outliers = None
+    global_data = pd.concat(data_frames, ignore_index=True)
+    global_series = global_data['Load']
+    global_outliers = detect_outliers(global_series, threshold=2.0)
 
-    if uploaded_folder:
-        global_data = pd.DataFrame()
-        for file_path in file_paths:
-            df = read_excel_file(file_path)
-            global_data = global_data.append(df, ignore_index=True)
-
-        # Utiliser la fonction de détection des outliers pour la courbe globale
-        global_series = global_data['Load']
-        global_outliers = detect_outliers(global_series, threshold=2.0)
-
-    if global_outliers is not None:
+    if not global_outliers.empty:
         st.write("Outliers pour la courbe globale :")
         st.write(global_outliers)
+
+        # Afficher les valeurs aberrantes avec leurs dates correspondantes
+        outliers_with_datetime = global_data[global_data['Load'].isin(global_outliers)]
+        st.write("Valeurs aberrantes avec leur date et heure correspondantes:")
+        st.write(outliers_with_datetime[['Datetime', 'Load']])
